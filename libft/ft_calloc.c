96cd5f43f5e1054ec0b156f464cee0eda5e382b6
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-21 11:34:21 +0200

    Refactor calloc function

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,18 +6,24 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 10:41:14 by vbcvali           #+#    #+#             */
-/*   Updated: 2024/09/21 11:04:21 by vbcvali          ###   ########.fr       */
+/*   Updated: 2024/09/21 11:33:08 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void *ft_calloc(size_t nmemb, size_t size)
 {
-	void    *ptr;
-	
-	if (!(ptr = (void *)malloc(size * nmemb)))
+	void	*ptr;
+	size_t	total_size;
+
+	total_size = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return NULL;
+	else if (size != 0 && (total_size / size) != nmemb)
+		return NULL;
+	else if (!(ptr = malloc(total_size)))
 		return (NULL);
-	ft_bzero(ptr, size * nmemb);
+	ft_bzero(ptr, total_size);
 	return (ptr);
 }
