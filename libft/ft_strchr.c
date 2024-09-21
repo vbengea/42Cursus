5311321c56276be2fc581dd30dd42d03e358c5f8
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-21 10:32:12 +0200

    Added library

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,9 +6,11 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:17:08 by vbcvali           #+#    #+#             */
-/*   Updated: 2024/09/19 20:19:06 by vbcvali          ###   ########.fr       */
+/*   Updated: 2024/09/21 10:30:01 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char    *ft_strchr(const char *s, int c)
 {
@@ -23,14 +25,14 @@ char    *ft_strchr(const char *s, int c)
     return (NULL);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-int main(void)
-{
-    const char *test = "Valentin";
-    int c = 'l';
+// int main(void)
+// {
+//     const char *test = "Valentin";
+//     int c = 'l';
 
-    char *result = ft_strchr(test, c);
-    printf("%p\n", result);
-}
+//     char *result = ft_strchr(test, c);
+//     printf("%p\n", result);
+// }
