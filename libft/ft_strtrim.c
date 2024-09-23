3c0840bcfb1ea5de8a10184387a2a5dbbd2a70b0
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-23 16:52:51 +0200

    Added strtrim function

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 16:08:25 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/23 16:52:24 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+
+	if (!set || !s1)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1 != '\0')
+		s1++;
+	i = ft_strlen((char *)s1);
+	while (ft_strchr(set, *s1) && i != '\0')
+		i--;
+	return(ft_substr((char *)s1, 0 i + 1));
+}
