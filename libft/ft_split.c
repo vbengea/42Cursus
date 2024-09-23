8cc625ca83fd96646f387147fb077000a56d583d
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-23 19:44:29 +0200

    Added split function

---
+++ libft/ft_split.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 16:53:31 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/23 19:42:54 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	i = 0;
+	subs = (char *)malloc(sizeof(char) * (len + 1));
+	if (!(subs))
+		return (NULL);
+	while (start < len)
+	{
+		subs[i] = s[start];
+		i++;
+		start++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
+
+
+static int  count_words(char const *s, char c)
+{
+    int count = 0;
+    int in_word = 0;
+
+    while (*s)
+    {
+        if (*s != c && !in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+        else if (*s == c)
+            in_word = 0;
+        s++;
+    }
+    return (count);
+}
+
+static  int get_word_len(char const *s, char *sep)
+{
+    int i;
+
+    i = 0;
+    while (s[i] != *sep && s[i] != '\0') 
+        i++;
+    return (i);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    char **split_array;
+    int i;
+    int j;
+    int word_len;
+
+    split_array = malloc(sizeof(char *) * (count_words(s, c) + 1));
+    if (!split_array)
+        return (NULL);
+    i = 0;
+    while (*s)
+    {
+        while (*s == c)
+            s++;
+        if (*s)
+        {
+            word_len = get_word_len(s, &c);
+            split_array[i] = ft_substr(s, 0, word_len);
+            if (!split_array[i])
+                return (NULL); // Handle memory leak in real code
+            s += word_len;
+            i++;
+        }
+    }
+    split_array[i] = NULL;
+    return (split_array);
+}
+
+// int main(void)
+// {
+//     const char *t = "hello world again";
+//     char **p = ft_split(t, ' ');
+//     printf("%s\n", p[0]);
+//     printf("%s\n", p[1]);
+//     printf("%s\n", p[2]);
+//     return 0; 
+// }
