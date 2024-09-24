91e88322c8542a53bf9d77e2fe5ae3c195732532
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-24 11:41:01 +0200

    Added putnbr function

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 11:31:21 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/24 11:40:33 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_putnbr_fd(int n, int fd)
+{
+	char	num_char;
+
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
+	{
+		n = n * -1;
+		write(fd, "-", 1);
+		if (n >= 10)
+			ft_putnbr(n / 10);
+		num_char = (n % 10) + '0';
+		write(fd, &num_char, 1);
+	}
+	else
+	{
+		if (n >= 10)
+			ft_putnbr(n / 10);
+		num_char = (n % 10) + '0';
+		write(fd, &num_char, 1);
+	}
+}
