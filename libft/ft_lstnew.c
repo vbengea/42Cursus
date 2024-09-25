45fa2c0a8aa4e88027990be13f2f59068ba320bf
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-25 17:44:08 +0200

    Added lstnew function

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 17:29:16 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/25 17:38:53 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!(new))
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
