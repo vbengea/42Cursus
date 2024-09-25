3dafb5303da76c0695cf76205f10d1a58d653368
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-25 18:45:07 +0200

    Added lstadd_back function

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 18:35:50 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/25 18:43:38 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
