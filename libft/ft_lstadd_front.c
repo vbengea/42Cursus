e38e05e5308fb580f2071e4b15f2ea14638ad8cb
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-09-25 18:09:31 +0200

    Added ft_lstadd_front function

---
+++ libft/ft_lstadd_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 17:44:47 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/09/25 18:07:55 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
+
+// new points to content of lst
+// lst point to new
