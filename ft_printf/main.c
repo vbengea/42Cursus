0c86b13a7675e33c130f2c1fa236ddbf151a0666
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-10-11 19:52:16 +0200

    Added main function to test ft_printf

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,9 +6,12 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 12:21:37 by vbcvali           #+#    #+#             */
-/*   Updated: 2024/10/06 12:41:19 by vbcvali          ###   ########.fr       */
+/*   Updated: 2024/10/11 19:43:07 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_printf.h"
 
 int main()
 {
@@ -23,10 +26,10 @@ int main()
 	ft_printf("This is a %%\n");
 	ft_printf("This is a %c %s %d %x %X %p %%\n", 'c', "string", 42, 42, 42, "pointer");
 
-	int count = ft_printf("This is an unsigned int: %u\n", 42);
-	printf("count: %d\n", count);
-	count = printf("This is an unsigned int: %u\n", 42);
-	printf("count: %d\n", count);
+	// int count = ft_printf("This is an unsigned int: %u\n", 42);
+	// printf("count: %d\n", count);
+	// count = printf("This is an unsigned int: %u\n", 42);
+	// printf("count: %d\n", count);
 
 
 	ft_printf(" %% ");
@@ -43,7 +46,22 @@ int main()
 	ft_printf("\n");
 	ft_printf("%% %%");
 	ft_printf("\n");
-
+	ft_printf("Hola que tal esto es un texto de prueba%");
+	//printf("Hola que tal esto es un texto de prueba%");
+	ft_printf("%%");
 
 	return (0);
 }
+
+// int main()
+// {
+//     int result;
+
+//     result = ft_printf("Hello, world!\n");
+//     printf("Result: %d\n", result);
+
+//     result = ft_printf("Incomplete format: %\n");
+//     printf("Result: %d\n", result);
+
+//     return 0;
+// }
