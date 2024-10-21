173baa63e42b1eee95a99e48d176cc10d095826f
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-10-21 19:30:30 +0200

    Added stack init

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 17:29:03 by vbcvali           #+#    #+#             */
-/*   Updated: 2024/10/19 17:31:19 by vbcvali          ###   ########.fr       */
+/*   Updated: 2024/10/21 16:19:40 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,20 @@ typedef struct  s_list
     void *content;
     struct s_list *next;
 }   t_list;
+
+void    pa(t_list **a, t_list **b);
+void    pb(t_list **a, t_list **b);
+
+void    rra(t_list **a);
+void    rrb(t_list **b);
+void    rrr(t_list **a, t_list **b);
+
+void    sa(t_list **a);
+void    sb(t_list **b);
+void    ss(t_list **a, t_list **b);
+
+void    ra(t_list **a);
+void    rb(t_list **b);
+void    rr(t_list **a, t_list **b);
 
 #endif
---
+++ push_swap/stack_init.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 17:29:22 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/10/21 17:37:07 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap_temp.h"
+
+static long	ft_atol(const char *str)
+{
+	long	nbr;
+	int		sign;
+
+	nbr = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		nbr = (nbr * 10) + (*str - '0');
+		str++;
+	}
+	return (nbr * sign);
+}
+
+void	stack_init(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
+			error_free(a, argv, flag_argc_2);
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error_free(a, argv, flag_argc_2);
+		if (error_repetition(*a, (int)nbr))
+			error_free(a, argv, flag_argc_2);
+		append_node(a, (int)nbr);
+		i++;
+	}
+	if (flag_argc_2)
+		free_matrix(argv);
+}
