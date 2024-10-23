3338e896499aaf440b4211f0df8227394dcca2c3
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-10-23 10:38:08 +0200

    Added function to split arguments

---
+++ push_swap/split.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 09:56:09 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/10/23 10:20:44 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	count_words(char *s, char c)
+{
+	int		count;
+	bool	in_word;
+
+	count = 0;
+	while (*s)
+	{
+		in_word = false;
+		while (*s == c)
+			s++;
+		while (*s != c && *s)
+		{
+			if (!in_word)
+			{
+				count++;
+				in_word = true;
+			}
+			s++;
+		}
+	}
+	return (count);
+}
+
+static char	*get_next_word(char *s, char c)
+{
+	static int	cursor = 0;
+	char		*next_word;
+	int			len;
+	int			i;
+
+	len = 0;
+	i = 0;
+	while (s[cursor] == c)
+		cursor++;
+	while ((s[cursor + len] != c) && s[cursor + len])
+		len++;
+	next_word = malloc((len * sizeof(char)) + 1);
+	if (!next_word)
+		return (NULL);
+	while ((s[cursor] != c) && s[cursor])
+		next_word[i++] = s[cursor++];
+	next_word[i] = '\0';
+	return (next_word);
+}
+
+char	**split(char *s, char c)
+{
+	char	**array;
+	int		word_count;
+	int		i;
+
+	i = 0;
+	word_count = count_words(s, c);
+	if (!word_count)
+		exit(1);
+	array = malloc(sizeof(char *) * (word_count + 2));
+	if (!array)
+		return (NULL);
+	while (word_count-- >= 0)
+	{
+		if (i == 0)
+		{
+			array[i] = malloc(sizeof(char));
+			if (!array[i])
+				return (NULL);
+			array[i++][0] = '\0';
+			continue ;
+		}
+		array[i++] = get_next_word(s, c);
+	}
+	array[i] = NULL;
+	return (array);
+}
