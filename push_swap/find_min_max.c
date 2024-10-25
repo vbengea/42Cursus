5d82afb97fff1321213298e61d8a1373bd66fcb3
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-10-25 13:28:08 +0200

    Added find MAX/MIN & started push_swap

---
+++ push_swap/find_min_max.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_min_max.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 12:31:34 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/10/25 12:35:58 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack_node	*find_min(t_stack_node *stack)
+{
+	long			min;
+	t_stack_node	*min_node;
+
+	if (!stack)
+		return (NULL);
+	min = LONG_MAX;
+	while (stack)
+	{
+		if (stack->value < min)
+		{
+			min = stack->value;
+			min_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (min_node);
+}
+
+t_stack_node	*find_max(t_stack_node *stack)
+{
+	long			max;
+	t_stack_node	*max_node;
+
+	if (!stack)
+		return (NULL);
+	max = LONG_MAX;
+	while (stack)
+	{
+		if (stack->value > max)
+		{
+			max = stack->value;
+			max_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_node);
+}
---
+++ push_swap/push_swap.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 11:15:16 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/10/25 13:15:47 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static	void	move_a_to_b(t_stack_node **a, t_stack_node **b)
+{
+	t_stack_node	*cheapest;
+
+	cheapest = get_cheapest(*a);
+	if (cheapest->above_median && cheapest->target_node->above_median)
+		rotate(a, b, cheapest);
+	else if (!(cheapest->above_median)
+		&& !(cheapest->target_node->above_median))
+		reverse_rotate(a, b, cheapest);
+	prep_for_push(a, cheapest, 'a');
+	prep_for_push(b, cheapest->target_node, 'b');
+	pb(a, b);
+}
+
+static	void	move_b_to_a(t_stack_node **a, t_stack_node **b)
+{
+	prep_for_push(a, (*b)->target_node, 'a');
+	pa(a, b);
+}
+
+static	void	min_on_top(t_stack_node **a)
+{
+	while ((*a)->value != find_min(*a)->value)
+	{
+		if (find_min(*a)->above_median)
+			ra(a);
+		else
+			rra(a);
+	}
+}
+
+void	push_swap(t_stack_node **a, t_stack_node **b)
+{
+	int	len;
+
+	len = stack_len(a);
+	if (len-- > 3 && !stack_sorted(a))
+		pb(a, b);
+	if (len-- > 3 && !stack_sorted(a))
+		pb(a, b);
+	while (len-- > 3 && !stack_sorted(a))
+	{
+		init_nodes(*a, *b);
+		move_a_to_b(a, b);
+	}
+	sort_three(a);
+	while (*b)
+	{
+		init_nodes_b(*a, *b);
+		move_b_to_a(a, b);
+	}
+	current_position(*a);
+	min_on_top(a);
+}
