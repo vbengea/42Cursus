f7db0816228a9d2754fcb3139414d53537982462
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-10-28 20:18:52 +0100

    Added function to put a node from a stack on top

---
+++ push_swap/put_node_on_top.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_node_on_top.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 19:48:01 by vbcvali           #+#    #+#             */
+/*   Updated: 2024/10/28 19:54:57 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	put_node_on_top(t_stack_node **stack,
+		t_stack_node *top, char stack_name)
+{
+	t_stack_node	*temp;
+
+	temp = *stack;
+	while (temp != top)
+	{
+		if (stack_name == 'a')
+		{
+			if (top->above_median)
+				ra(temp);
+			else
+				rra(temp);
+		}
+		else if (stack_name == 'b')
+		{
+			if (top->above_median)
+				ra(temp);
+			else
+				rra(temp);
+		}
+		temp = temp->next;
+	}
+}
