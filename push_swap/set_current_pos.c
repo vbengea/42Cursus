29bb4495db0a28e427e65c6532ed27cd118e358c
Valentin Catalin Bengea <vbengea@student.42madrid.com>
2024-10-28 10:14:20 +0100

    Added funciton for indexing nodes

---
+++ push_swap/set_current_pos.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_current_pos.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbengea <vbengea@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 08:42:06 by vbengea           #+#    #+#             */
+/*   Updated: 2024/10/28 10:05:58 by vbengea          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	set_current_pos(t_stack_node **stack)
+{
+	t_stack_node	*temp;
+	int				median;
+	int				i;
+
+	temp = *stack;
+	i = 0;
+	median = stack_len(&temp) / 2;
+	while (temp)
+	{
+		temp->current_position = i;
+		if (i < median)
+			temp->above_median = false;
+		else
+			temp->above_median = true;
+		temp = temp->next;
+		i++;
+	}
+}
