43b7268abb58d986d9cfec1cc98c68e925f57777
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-11-17 13:02:15 +0100

    Initialize struct to avoid valgrind errors

--- so_long/minilibx-linux/mlx_int_anti_resize_win.c
+++ so_long/minilibx-linux/mlx_int_anti_resize_win.c
@@ -11,18 +11,38 @@
 #include	"mlx_int.h"
 
 
-int	mlx_int_anti_resize_win(t_xvar *xvar,Window win,int w,int h)
+// int	mlx_int_anti_resize_win(t_xvar *xvar,Window win,int w,int h)
+// {
+//   XSizeHints    hints;
+//   long		toto;
+	
+//   XGetWMNormalHints(xvar->display,win,&hints,&toto);
+//   hints.width = w;
+//   hints.height = h;
+//   hints.min_width = w;
+//   hints.min_height = h;
+//   hints.max_width = w;
+//   hints.max_height = h;
+//   hints.flags = PPosition | PSize | PMinSize | PMaxSize;
+//   XSetWMNormalHints(xvar->display,win,&hints);
+// }
+
+
+int mlx_int_anti_resize_win(t_xvar *xvar, Window win, int w, int h)
 {
-  XSizeHints    hints;
-  long		toto;
-  
-  XGetWMNormalHints(xvar->display,win,&hints,&toto);
-  hints.width = w;
-  hints.height = h;
-  hints.min_width = w;
-  hints.min_height = h;
-  hints.max_width = w;
-  hints.max_height = h;
-  hints.flags = PPosition | PSize | PMinSize | PMaxSize;
-  XSetWMNormalHints(xvar->display,win,&hints);
+		XSizeHints hints;
+		long toto;
+
+		// Initialize the hints structure to zero
+		memset(&hints, 0, sizeof(hints));
+
+		XGetWMNormalHints(xvar->display, win, &hints, &toto);
+		hints.width = w;
+		hints.height = h;
+		hints.min_width = w;
+		hints.min_height = h;
+		hints.max_width = w;
+		hints.max_height = h;
+		hints.flags = PPosition | PSize | PMinSize | PMaxSize;
+		XSetWMNormalHints(xvar->display, win, &hints);
 }
