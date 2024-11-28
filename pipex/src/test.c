d43a796e3667d6c40a1fdf8fd5cc09a5f3d694e4
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-11-28 19:02:25 +0100

    Created file structure & Makefile

--- pipex/src/test.c
+++ pipex/src/test.c
@@ -59,68 +59,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include "libft/libft.h"
+#include "../incl/libft/libft.h"
 
 void execute_command(char *cmd[], int input_fd, int output_fd) {
-    if (input_fd != STDIN_FILENO) {
-        dup2(input_fd, STDIN_FILENO);
-        close(input_fd);
-    }
-    if (output_fd != STDOUT_FILENO) {
-        dup2(output_fd, STDOUT_FILENO);
-        close(output_fd);
-    }
-    execve(cmd[0], cmd, NULL);
-    perror("execve");
-    exit(1);
+	if (input_fd != STDIN_FILENO) {
+		dup2(input_fd, STDIN_FILENO);
+		close(input_fd);
+	}
+	if (output_fd != STDOUT_FILENO) {
+		dup2(output_fd, STDOUT_FILENO);
+		close(output_fd);
+	}
+	printf("Test working !\n");
+	execve(cmd[0], cmd, NULL);
+	perror("execve");
+	exit(1);
 }
 
 int main() {
-    int num_commands = 3;
-    char *commands[3][3] = {
-        {"/bin/ls", "-l", NULL},
-        {"/usr/bin/grep", "main", NULL},
-        {"/usr/bin/wc", "l", NULL}
-    };
+	int num_commands = 3;
+	char *commands[3][3] = {
+		{"/bin/ls", "-la", NULL},
+		{"/usr/bin/grep", "main", NULL},
+		{"/usr/bin/wc", "-l", NULL}
+	};
 
-    int pipefd[2];
-    int input_fd = STDIN_FILENO;
+	int pipefd[2];
+	int input_fd = STDIN_FILENO;
 
-    for (int i = 0; i < num_commands; i++) {
-        if (i < num_commands - 1) {
-            if (pipe(pipefd) == -1) {
-                perror("pipe");
-                return 1;
-            }
-        } else {
-            pipefd[1] = STDOUT_FILENO;
-        }
+	for (int i = 0; i < num_commands; i++) {
+		if (i < num_commands - 1) {
+			if (pipe(pipefd) == -1) {
+				perror("pipe");
+				return 1;
+			}
+		} else {
+			pipefd[1] = STDOUT_FILENO;
+		}
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            return 1;
-        }
+		pid_t pid = fork();
+		if (pid < 0) {
+			perror("fork");
+			return 1;
+		}
 
-        if (pid == 0) {
-            if (i < num_commands - 1) {
-                close(pipefd[0]);
-            }
-            execute_command(commands[i], input_fd, pipefd[1]);
-        } else {
-            if (input_fd != STDIN_FILENO) {
-                close(input_fd);
-            }
-            if (i < num_commands - 1) {
-                close(pipefd[1]);
-                input_fd = pipefd[0];
-            }
-        }
-    }
+		if (pid == 0) {
+			if (i < num_commands - 1) {
+				close(pipefd[0]);
+			}
+			execute_command(commands[i], input_fd, pipefd[1]);
+		} else {
+			if (input_fd != STDIN_FILENO) {
+				close(input_fd);
+			}
+			if (i < num_commands - 1) {
+				close(pipefd[1]);
+				input_fd = pipefd[0];
+			}
+		}
+	}
 
-    for (int i = 0; i < num_commands; i++) {
-        wait(NULL);
-    }
+	for (int i = 0; i < num_commands; i++) {
+		wait(NULL);
+	}
 
-    return 0;
+	return 0;
 }
