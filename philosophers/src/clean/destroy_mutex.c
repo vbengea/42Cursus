1a78be1259cfa8693b3062a8de7b891f7d2f445f
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-01-02 20:28:46 +0100

    Change the way mutexes where destroyed

--- philosophers/src/clean/destroy_mutex.c
+++ philosophers/src/clean/destroy_mutex.c
@@ -6,7 +6,7 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 12:29:08 by vbcvali           #+#    #+#             */
-/*   Updated: 2025/01/01 12:35:15 by vbcvali          ###   ########.fr       */
+/*   Updated: 2025/01/02 19:15:15 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ void	destroy_mutex(t_data *data)
 	int	i;
 
 	i = 0;
-	if (data->forks)
-	{
-		while (i < (int)data->n_philos)
-			pthread_mutex_destroy(&data->forks[i++]);
-	}
+	while (i < (int)data->n_philos)
+		pthread_mutex_destroy(&data->forks[i++]);
+	pthread_mutex_destroy(&data->stop_mutex);
+	pthread_mutex_destroy(&data->philos->status_lock);
 }
+// TO DO
+
+/* Update a flag when initializing a mutex to know when to destroy it
+	There is no official way to check if a mutex has been initialized and 
+	destroying an unitialized mutex can lead to undefined behavior */
