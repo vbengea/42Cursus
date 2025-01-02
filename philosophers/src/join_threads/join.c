e9ec0e51766ee2a30c055b2a4d770dd0363d17ff
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-01-02 20:23:18 +0100

    Move join threads in a separate function

---
+++ philosophers/src/join_threads/join.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   join.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 19:04:59 by vbcvali           #+#    #+#             */
+/*   Updated: 2025/01/02 19:07:16 by vbcvali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/philosophers.h"
+
+int	join_threads(t_data *data)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < data->n_philos)
+	{
+		if (pthread_join(data->philos[i].thread, NULL) != 0)
+			return (1);
+		i++;
+	}
+	if (pthread_join(data->monitor, NULL) != 0)
+		return (1);
+	return (0);
+}
