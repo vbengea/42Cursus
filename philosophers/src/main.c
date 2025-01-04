a3f2aa9f97ea5f36c4e806292fbd5a6e193f7563
Valentin Catalin Bengea <valentinbengea@gmail.com>
2025-01-04 20:21:18 +0100

    Update running order

--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vbcvali <vbcvali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 12:53:51 by vbcvali           #+#    #+#             */
-/*   Updated: 2025/01/04 11:54:59 by vbcvali          ###   ########.fr       */
+/*   Updated: 2025/01/04 13:23:30 by vbcvali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,15 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	if (valid_input(argv))
+	{
 		init_program(&data, argc, argv);
-	destroy_mutex(&data);
-	free_all(&data);
+		destroy_mutex(&data);
+		free_all(&data);
+	}
+	else
+	{
+		printf("Input out of range!\n");
+		return (1);
+	}
 	return (0);
 }
